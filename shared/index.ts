77c9e4ac5d5a8912ece0929b7022c2d4d3943162
Ben Bolthouse <benbolthouse@gmail.com>
2023-10-09 02:38:27 -0700

    Move entrypoint to src

--- shared/index.ts
+++
@@ -1,5 +0,0 @@
-export * as Clients from "./src/clients";
-export * as Hooks from "./src/hooks";
-export * as Models from "./src/models";
-export * as Types from "./src/types";
-export { default as Cache } from "./src/cache";
---
+++ shared/src/index.ts
@@ -0,0 +1,4 @@
+export * as Clients from "./clients";
+export * as Models from "./models";
+export * as Types from "./types";
+export { default as Cache } from "./cache";
