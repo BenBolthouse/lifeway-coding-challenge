49b90b71490eadb348527cbf781bf794262120b8
Ben Bolthouse <benbolthouse@gmail.com>
2023-10-09 01:08:03 -0700

    Delete js files

--- shared/src/models/swapi/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ResourceType = void 0;
-var Base_1 = require("./Base");
-Object.defineProperty(exports, "ResourceType", { enumerable: true, get: function () { return Base_1.ResourceType; } });
--- shared/src/utilities/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Resource = void 0;
-exports.Resource = require("./ResourceUtility");
