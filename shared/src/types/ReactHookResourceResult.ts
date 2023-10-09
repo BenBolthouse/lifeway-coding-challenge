b949dcd9713a023b8836f546783307c3cd400821
Ben Bolthouse <benbolthouse@gmail.com>
2023-10-09 02:40:43 -0700

    Remove React related content from shared

--- shared/src/types/ReactHookResourceResult.ts
+++ shared/src/types/ReactHookResourceResult.ts
@@ -1,1 +1,1 @@
-export type ReactHookResourceResult<TArgs, TModel> = [execute: (args: TArgs) => void, pending: boolean, error: boolean, data: TModel]; // eslint-disable-line
+export type ReactHookResourceResult<TModel> = [pending: boolean, error: boolean, data: TModel];
